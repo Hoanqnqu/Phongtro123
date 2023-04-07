1acddedbd5060ade29166bd1b4c447b7af20c8a5
Hoanqnqu <nhathoangthuychaub1@gmail.com>
2023-04-08 02:16:55 +0700

    get Province

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,22 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SelectAddress from './SelectAddress';
-
+import { apiGetPublicDistricts, apiGetPublicProvinces } from '~/services';
 const Address = () => {
+    const [provinces, setProvinces] = useState([]);
+    const [province, setProvince] = useState();
+    const [district, setDistrict] = useState();
+    const [districts, setDistricts] = useState([]);
+    const [reset, setReset] = useState(false);
+
+    useEffect(() => {
+        const fetchPublicProvince = async () => {
+            const response = await apiGetPublicProvinces();
+            if (response.status === 200) setProvinces(response?.data?.results);
+        };
+        fetchPublicProvince();
+    }, []);
+    useEffect(() => {
+        setDistrict(null);
+        const fetchPublicDistrict = async () => {
+            const response = await apiGetPublicDistricts(province);
+            if (response.status === 200) setDistricts(response?.data?.results);
+        };
+
+        if (province) {
+            setReset(false);
+            fetchPublicDistrict();
+        }
+        if (!province) {
+            setReset(true);
+            setDistricts([]);
+        }
+    }, [province]);
     return (
         <div>
             <h2 className="font-semibold text-xl py-4">Địa chỉ cho thuê</h2>
             <div className=" flex flex-col gap-4">
                 <div className="flex  items-center gap-4">
-                    <SelectAddress label={'Tỉnh/ Thành phố'} />
-                    <SelectAddress label={'Quận/ Huyện'} />
+                    <SelectAddress
+                        type="province"
+                        value={province}
+                        setValue={setProvince}
+                        label={'Tỉnh/ Thành phố'}
+                        options={provinces}
+                    />
+                    <SelectAddress
+                        reset={reset}
+                        type={'district'}
+                        value={district}
+                        setValue={setDistrict}
+                        label={'Quận/ Huyện'}
+                        options={districts}
+                    />
                 </div>
             </div>
-            <input
-                type="text"
-                value={'123'}
-                readOnly
-                className="border border-gray-200 rounded-md outline-none bg-gray-100 p-2 w-full"
-            />
+            <div className="flex flex-col gap-2">
+                <label htmlFor="exactly-address" className="font-medium">
+                    Địa chỉ chính xác
+                </label>
+                <input
+                    id="exactly-address"
+                    type="text"
+                    readOnly
+                    value={`${
+                        district
+                            ? `${
+                                  districts.find((districtItem) => districtItem?.district_id === district).district_name
+                              },`
+                            : ''
+                    } ${province ? `${provinces?.find((item) => item.province_id === province).province_name} ` : ''}`}
+                    className="border border-gray-200 rounded-md outline-none bg-gray-100 p-2 w-full"
+                />
+            </div>
         </div>
     );
 };
--- src/services/app.js
+++ src/services/app.js
@@ -1,5 +1,5 @@
 import axiosConfig from '~/axiosConfig';
-
+import AxiosDefault from 'axios';
 export const apiGetPrices = () =>
     new Promise(async (resolve, reject) => {
         try {
@@ -30,6 +30,30 @@ export const apiGetProvinces = () =>
             const response = await axiosConfig({
                 method: 'get',
                 url: 'api/v1/province/all',
+            });
+            resolve(response);
+        } catch (error) {
+            reject(reject);
+        }
+    });
+export const apiGetPublicProvinces = () =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const response = await AxiosDefault({
+                method: 'get',
+                url: 'https://vapi.vnappmob.com/api/province/',
+            });
+            resolve(response);
+        } catch (error) {
+            reject(reject);
+        }
+    });
+export const apiGetPublicDistricts = (province_id) =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const response = await AxiosDefault({
+                method: 'get',
+                url: `https://vapi.vnappmob.com/api/province/district/${province_id}`,
             });
             resolve(response);
         } catch (error) {
